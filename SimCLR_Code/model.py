ba2b8586285933f057b251e43305b4a5e5c5ac10
JunhuaL <linjunhua3@gmail.com>
2023-03-13 22:38:54 +0000

    gpu random

--- SimCLR_Code/model.py
+++ SimCLR_Code/model.py
@@ -14,7 +14,7 @@ class GaussianNoise:
         self.mean = mean
 
     def __call__(self,tensor):
-        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+        return tensor + torch.FloatTensor(tensor.size()).normal_() * self.std + self.mean
     
     def __repr__(self):
         return self.__class__.__name__ + f'(mean={self.mean},std={self.std})'
